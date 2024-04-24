f0d2ef742711f768bfa099e94bc1f77aa95ec5c2
Ahmed Gamal <gemi6276@gmail.com>
2024-04-24 08:59:35 +0200

    Client setup and Product interface

---
+++ Client/src/app/models/Products.ts
@@ -0,0 +1,12 @@
+
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  pictureUrl: string;
+  price: number;
+  productBrandId: number;
+  productBrand: string;
+  productTypeId: number;
+  productType: string;
+}
